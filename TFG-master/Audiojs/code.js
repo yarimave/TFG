c58ad532f5210e8978ade6da53356ec32f9f1ed1
Yari <y.vega.martinez@avanade.com>
2019-05-02 09:42:52 +0200

    convolve running

--- TFG-master/Audiojs/code.js
+++ TFG-master/Audiojs/code.js
@@ -6,9 +6,10 @@ window.AudioContext = (function(){
 var azimuth = (Math.PI)/2;
 var elevation = 0;
 
-var audioURL = "thecatalyst.wav";
+var audioURL = "thecatalyst48.wav";
 var speakersBuffer = [];
 var audioData = null;
+var speaker;
 var source;
 
 var a = 0.125;
@@ -33,17 +34,39 @@ var ctx;
 // });
 var audioContext = new AudioContext();
 
-var convolver = audioContext.createConvolver();
-var songBuffer = audioContext.createBuffer(1, 10*audioContext.sampleRate, audioContext.sampleRate);
-var speakersBuffer = audioContext.createBuffer(8, 2*audioContext.sampleRate, audioContext.sampleRate);
+//var songBuffer = audioContext.createBuffer(1, 10*audioContext.sampleRate, audioContext.sampleRate);
+//var speakersBuffer = audioContext.createBuffer(8, 2*audioContext.sampleRate, audioContext.sampleRate);
 
-var saveInThisBuffer = function(buffer){
+var songBuffer = function(buffer){
   audioData = buffer;
 }
 
-loadSound(audioURL, saveInThisBuffer);
+var speakersBuffer = function(buffer){
+  speaker = buffer;
+}
 
-//loadSound("E35_A45.wav", speakersBuffer[0]);
+loadSound(audioURL, songBuffer);
+
+loadSound("E35_A4548.wav", speakersBuffer);
+
+setTimeout(function(){
+
+  arraySong = audioData.getChannelData(0);
+  speakerir = speaker.getChannelData(0);
+
+  output = conv(arraySong,speakerir);
+
+  var convolver = audioContext.createConvolver();
+  convolver.buffer = speaker;
+  var songSource = audioContext.createBufferSource();
+  songSource.buffer = audioData;
+  
+  var output = audioContext.createAnalyser();
+  songSource.connect(convolver).connect(output);
+},2000);
+
+
+
 //loadSound("E35_A-45.wav", speakersBuffer[1]);
 //loadSound("E-35_A45.wav", speakersBuffer[2]);
 //loadSound("E-35_A-45.wav", speakersBuffer[3]);
@@ -52,6 +75,8 @@ loadSound(audioURL, saveInThisBuffer);
 //loadSound("E-35_A135.wav", speakersBuffer[6]);
 //
 //loadSound("E-35_A-135.wav", speakersBuffer[7]);
+
+
 document.getElementById('stopbutton').disabled = true;
 
 document.getElementById('playbutton').addEventListener('click', function() {
@@ -70,8 +95,22 @@ document.getElementById('stopbutton').addEventListener('click', function() {
   document.getElementById('stopbutton').disabled = true;
 });
 
-arraySong = songBuffer.getChannelData(0);
+
+
+
+
+
+
+
+
+
+
+
+arraySong = audioData.getChannelData(0);
 //speaker1 = speakersBuffer.getChannelData(1);
+
+
+
 
 var W = arraySong;
 //var X = arraySong * Math.cos(azimuth) * Math.cos(elevation);
@@ -139,4 +178,26 @@ function multiplyTwo(vector, scalar1, scalar2){
     return element*scalar1*scalar2;
   });
   return new_vector;
+}
+
+function conv(vec1, vec2){
+    var disp = 0; // displacement given after each vector multiplication by element of another vector
+    var convVec = [];
+    // for first multiplication
+    for (j = 0; j < vec2.length ; j++){
+        convVec.push(vec1[0] * vec2[j]);
+    }
+    disp = disp + 1;
+    for (i = 1; i < vec1.length ; i++){
+        for (j = 0; j < vec2.length ; j++){
+            if ((disp + j) !== convVec.length){
+                convVec[disp + j] = convVec[disp + j] + (vec1[i] * vec2[j])
+            }
+            else{
+                convVec.push(vec1[i] * vec2[j]);
+            }
+        }
+        disp = disp + 1;
+    }
+    return convVec;
 }
